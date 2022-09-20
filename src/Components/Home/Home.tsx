a5af4a55a22cca6015a94ca2be31bf5a10e84d64
Prayas <prayas.jn24@gmail.com>
2022-09-20 11:27:49 +0530

    removed dropdown part 2

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,6 +1,5 @@
 import React, { useCallback, useState } from 'react'
 import './Home.scss';
-import 'react-dropdown/style.css';
 import Select from 'react-select';
 import info from '../../Images/info.png'
 import process_img from '../../Images/home-process.svg'
